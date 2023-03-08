6fe70cad703796a6f29dcdfd5a8898fe9b6d8b24
TherealBarbos <126061412+TherealBarbos@users.noreply.github.com>
2023-03-08 14:57:45 +0100

    lehere wunsch

--- Projekte/Liferando/script.js
+++ Projekte/Liferando/script.js
@@ -1,6 +1,9 @@
-let name = ["Tonkasu Ramen", "Spicy Miso Ramen", "Shio Ramen"];
-let price = ["11,95", "12.95", "13.95"]
-let cart = []
+let dishes =[
+    {"name":"Tonkasu Ramen", "priceChicken":"11,95","priceBeef":"12,95","priceShrimp":"13,95" }
+    {"name":"Spicy Miso Ramen", "priceChicken":"11,95","priceBeef":"12,95","priceShrimp":"13,95" }
+    {"name":"Shio Ramen", "priceChicken":"11,95","priceBeef":"12,95","priceShrimp":"13,95" }
+
+]
 
 // let priceChicken  = ["11,95","12.95","13.95"]
 // let priceBeef  = ["11,95","12.95","13.95"]
@@ -32,6 +35,39 @@ function render() {
     let content = document.getElementById('content');
     content.innerHTML = '';
     content.innerHTML = '';
+for (let i = 0; i < dishes.length; i++) {
+    const dish = dishes[i];
+    content.innerHTML += `
+    <div class="meal">
+                <img src="img/r1.png" alt="Menu">
+                <div class="orderTable">
+                    <h2>${dish['name']}</h2>
+                    <table style="width: 550px" class="bold">
+                        <tbody>
+                            <tr>
+                                <th style="width:70%">Chicken</th>
+                                <td style="width:20%">${dish['priceChicken']}</td>
+                                <td style="width:10%"><img src="img/plus-6-24.png" alt="" onclick="addtoCard()"
+                                        class="addButton"></td>
+                            </tr>
+
+                            <tr>
+                                <td>Beef</td>
+                                <td>${dish['priceBeef']}</td>
+                                <td><img src="img/plus-6-24.png" alt="" onclick="addtoCard()" class="addButton"></td>
+                            </tr>
+                            <tr>
+                                <td>Shrimp</td>
+                                <td>${dish['priceShrimp']}</td>
+                                <td><img src="img/plus-6-24.png" alt="" onclick="addtoCard()" class="addButton"></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>`
+    
+}
+
 }
 
 function addCard(name, price) {

048683b38ae1f481a2d87a3c1ebf195ef7c77cfa
Barbøs <126061412+TherealBarbos@users.noreply.github.com>
2023-03-26 10:33:30 +0200

    test

--- Projekte/Quizapp/questions.js
+++ Projekte/Quizapp/questions.js
@@ -2,7 +2,7 @@ let questions = [
     {
         "question": "Wie Heißt die Hauptsadt von Deutschland?",
         "answer_1": "Berlin",
-        "answer_2": "Hammburg",
+        "answer_2": "Hamburg",
         "answer_3": "Offenburg",
         "answer_4": "München",
         "right_answer": 1
@@ -11,16 +11,16 @@ let questions = [
         "question": "Welche Farben hat die Deutschlandflagge?",
         "answer_1": "Blau, Weis und Rot",
         "answer_2": "Gelb, Grün und Blau",
-        "answer_3": "Schwarz, Rot und Gelb ",
+        "answer_3": "Schwarz, Rot und Gold ",
         "answer_4": "Weis, Grün und Rot",
         "right_answer": 3
     },
     {
-        "question": "frage 3?",
-        "answer_1": "Berlin",
-        "answer_2": "Hammburg",
-        "answer_3": "Offenburg",
-        "answer_4": "München",
+        "question": "Wer ist der Boss?",
+        "answer_1": "ich",
+        "answer_2": "du",
+        "answer_3": "wer?",
+        "answer_4": "was?",
         "right_answer": 1
     },
     {

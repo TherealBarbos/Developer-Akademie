15d5324f2d8516a596243c9ea6b78e6b8f5201c7
TherealBarbos <126061412+TherealBarbos@users.noreply.github.com>
2023-04-02 16:07:32 +0200

    endscreen

--- Projekte/Quizapp/script.js
+++ Projekte/Quizapp/script.js
@@ -1,4 +1,5 @@
 let currentQuestion = 0;
+let rightQuestions =0 ;
 
 function init() {
     document.getElementById('all-questions').innerHTML = questions.length;
@@ -8,14 +9,31 @@ function init() {
 };
 
 function showQuestion() {
-    let question = questions[currentQuestion];
-    document.getElementById('curQuestion').innerHTML = currentQuestion + 1;
-    document.getElementById('questionText').innerHTML = question['question'];
-    document.getElementById('answer_1').innerHTML = question['answer_1'];
-    document.getElementById('answer_2').innerHTML = question['answer_2'];
-    document.getElementById('answer_3').innerHTML = question['answer_3'];
-    document.getElementById('answer_4').innerHTML = question['answer_4'];
-    ;
+
+    if (currentQuestion >= questions.length) {
+
+        document.getElementById('endScreen').style ='';
+        document.getElementById('qImg').style ='display: none';
+        document.getElementById('qBody').style ='display: none';
+        document.getElementById('qText').style ='display: none';
+        document.getElementById('qMenue').style ='display: none';
+
+        document.getElementById('qAmount').innerHTML = questions.length;
+        document.getElementById('qScore').innerHTML = rightQuestions;
+        document.getElementById('score').innerHTML = rightQuestions;
+
+    } else {
+
+        let question = questions[currentQuestion];
+
+        document.getElementById('curQuestion').innerHTML = currentQuestion + 1;
+        document.getElementById('questionText').innerHTML = question['question'];
+        document.getElementById('answer_1').innerHTML = question['answer_1'];
+        document.getElementById('answer_2').innerHTML = question['answer_2'];
+        document.getElementById('answer_3').innerHTML = question['answer_3'];
+        document.getElementById('answer_4').innerHTML = question['answer_4'];
+
+    }
 }
 
 function answer(selection) {
@@ -26,6 +44,7 @@ function answer(selection) {
     if (qNumber == question['right_answer']) {
         console.log('Richtig!')
         document.getElementById(selection).classList.add('bg-success')
+        rightQuestions++;
     } else {
         console.log('Falsch!')
         document.getElementById(selection).classList.add('bg-danger')
@@ -60,9 +79,8 @@ function resetBtn() {
     document.getElementById('answer_3').classList.remove('bg-success')
     document.getElementById('answer_4').classList.remove('bg-danger')
     document.getElementById('answer_4').classList.remove('bg-success')
-
-
-
 }
 
+function score() {
 
+}

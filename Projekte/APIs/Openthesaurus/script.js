26b9ce3948255c8c7ebfe79d10f3acae8db52216
TherealBarbos <126061412+TherealBarbos@users.noreply.github.com>
2023-04-24 17:28:36 +0200

    openthesaurus finisch

--- Projekte/APIs/Openthesaurus/script.js
+++ Projekte/APIs/Openthesaurus/script.js
@@ -1,26 +1,27 @@
 async function getSynonyms() {
-    let query = document.getElementById('searchQuery').value;
-    let url = `https://www.openthesaurus.de/synonyme/​search?q=${query}&format=applin/json`;
-    let response = await fetch(url);
-    let responseAsJson = await response.json();
-    let synsets = responseAsJson['synsets'];
-    renderSynsets(synsets);
+  let query = document.getElementById("searchQuery").value;
+  let url = `https://www.openthesaurus.de/synonyme/search?q=${query}&format=application/json`;
+  let response = await fetch(url);
+  // console.log('Response is', response);
+  let responseAsJson =await response.json();
+  // console.log('Json', responseAsJson);
+  let synsets = responseAsJson["synsets"];
+  renderSynsets(synsets);
 }
 
 function renderSynsets(synsets) {
-    let container = document.getElementById('container');
-    
-    container.innerHTML = `<div> Er wurden <b>${synsets.length}</b> Synonym-Sets geladen`;
+  let container = document.getElementById("container");
 
-    for (let i = 0; i < synsets.length; i++) {
-        const synset = synsets[i];
-        let terms = synset [`term`];
-        container.innerHTML += `<h2>Synonym-Set mit der ID ${synset['id']}</h2>`
-        
-        for (let j = 0; j < terms.length; j++) {
-            const term = terms[j];
-           container.innerHTML += `<div>${term['term']} `
-            
-        }
+  container.innerHTML = `<div> Er wurden <b>${synsets.length}</b> Synonym-Sets geladen`;
+
+  for (let i = 0; i < synsets.length; i++) {
+    const synset = synsets[i];
+    let terms = synset[`terms`];
+    container.innerHTML += `<h2>Synonym-Set mit der ID ${synset["id"]}</h2>`;
+
+    for (let j = 0; j < terms.length; j++) {
+      const term = terms[j];
+      container.innerHTML += `<div>${term["term"]} </div>`;
     }
+  }
 }

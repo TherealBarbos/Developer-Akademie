6c9ca076d6be51bc1c24fb389fe97811c70469d5
TherealBarbos <126061412+TherealBarbos@users.noreply.github.com>
2023-05-30 21:52:31 +0200

    Pokedex

--- Projekte/APIs/Pokedex/script.js
+++ Projekte/APIs/Pokedex/script.js
@@ -1,16 +1,30 @@
+function init(){
+  
+}
+
+
+
+
 async function loadPokemon() {
-    let url = 'https://pokeapi.co/api/v2/pokemon/bulbasaur';
-    let response = await fetch(url);
-    currentPokemon = await response.json();
-    console.log ('loaded', currentPokemon);
+  let url = "https://pokeapi.co/api/v2/pokemon/bulbasaur";
+  let response = await fetch(url);
+  currentPokemon = await response.json();
+  console.log("loaded", currentPokemon);
 
-    renderPokemon();
-
+  renderPokemon();
 }
 
 function renderPokemon() {
-    document.getElementById('pokeName').innerHTML = currentPokemon['name'];
-    document.getElementById('pokeNum').innerHTML = currentPokemon['order'];
-    document.getElementById('pokeImg').src = currentPokemon['sprites']['front_default'];
-    document.getElementById('pokeImgShiny').src = currentPokemon['sprites']['front_shiny'];
+  document.getElementById("pokeName").innerHTML = currentPokemon["name"];
+  document.getElementById("pokeNum").innerHTML = currentPokemon["order"];
+  document.getElementById("pokeImg").src =
+    currentPokemon["sprites"]["other"]["official-artwork"]["front_default"];
+  document.getElementById("typesOne").innerHTML =
+    currentPokemon["types"][0]["type"]["name"];
+  document.getElementById("typesTwo").innerHTML =
+    currentPokemon["types"][1]["type"]["name"];
+
+    document.getElementById("pokeHeight").innerHTML = currentPokemon["height"];
+    document.getElementById("pokeWeight").innerHTML = currentPokemon["weight"];
+    
 }

6e6413e0529dbaeac3ea39f02e00c09a7fd93d3d
Barbøs <126061412+TherealBarbos@users.noreply.github.com>
2023-10-26 14:14:17 +0200

    #

--- Projekte/Games/El Pollo Loco/assets/models/world.class.js
+++ Projekte/Games/El Pollo Loco/assets/models/world.class.js
@@ -1,12 +1,19 @@
 class World {
   character = new Character();
   enemies = [new Chicken(), new Chicken(), new Chicken()];
-  clouds = [ new Cloud()]
+  clouds = [new Cloud()];
+  background = [
+    new Background('img/5_background/layers/completed_background.png', 0),
+    new Background('img/5_background/layers/3_third_layer/full.png', 0),
+    new Background('img/5_background/layers/2_second_layer/full.png', 0),
+    new Background('img/5_background/layers/1_first_layer/full.png', 0),
+  ];
+  
   canvas;
   ctx;
 
   constructor(canvas) {
-    this.ctx = canvas.getContext('2d');
+    this.ctx = canvas.getContext("2d");
     this.canvas = canvas;
     this.draw();
   }
@@ -14,20 +21,36 @@ class World {
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-    this.ctx.drawImage(this.character.img, this.character.x,  this.character.y, this.character.width, this.character.height);
-    this.enemies.forEach(enemy => {
-      this.ctx.drawImage(enemy.img, enemy.x,  enemy.y, enemy.width, enemy.height);
+    this.ctx.drawImage(
+      this.character.img,
+      this.character.x,
+      this.character.y,
+      this.character.width,
+      this.character.height
+    );
+    this.enemies.forEach((enemy) => {
+      this.ctx.drawImage(
+        enemy.img,
+        enemy.x,
+        enemy.y,
+        enemy.width,
+        enemy.height
+      );
     });
 
-    this.clouds.forEach(cloud => {
-      this.ctx.drawImage(cloud.img, cloud.x,  cloud.y, cloud.width, cloud.height);
+    this.clouds.forEach((cloud) => {
+      this.ctx.drawImage(
+        cloud.img,
+        cloud.x,
+        cloud.y,
+        cloud.width,
+        cloud.height
+      );
     });
-
 
     let self = this;
-    requestAnimationFrame(function(){
+    requestAnimationFrame(function () {
       self.draw();
-
     });
   }
 }
